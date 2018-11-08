c19180325db997812d7297c5e6d67b53cb495518
purewind <qingfengbaozai@gmail.com>
2018-11-08 23:02:39 +0800

    添加logback进行对象构造

--- weixin-mp/src/main/java/com/riversoft/weixin/mp/template/Data.java
+++ weixin-mp/src/main/java/com/riversoft/weixin/mp/template/Data.java
@@ -1,8 +1,12 @@
 package com.riversoft.weixin.mp.template;
 
+import lombok.Builder;
+
 /**
  * Created by exizhai on 12/16/2015.
  */
+@Builder
+@lombok.Data
 public class Data {
 
     private String value;
@@ -11,24 +15,12 @@ public class Data {
     public Data() {
     }
 
+    public Data(String value) {
+        this.value = value;
+    }
     public Data(String value, String color) {
         this.value = value;
         this.color = color;
     }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
+
